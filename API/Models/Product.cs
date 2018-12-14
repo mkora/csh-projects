f2439f7aac4a3b566ec51359194ed6cada210caf
mkora <31717889+mkora@users.noreply.github.com>
2018-12-13 17:02:09 -0800

    Add api products controller

---
+++ API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace API.Models
+{
+
+    public class Product
+    {
+        public string Label { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
